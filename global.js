2b7fe555732231bec71ee4d4ed4bcac0f4f3c170
Son Nguyen <hpn005@ucsd.edu>
2025-04-25 12:19:12 -0700

    Complete Lab 04

--- global.js
+++ global.js
@@ -64,6 +64,7 @@ function updateDefinedColors(themeValue) {
 }
 
 
+
 select.addEventListener('input', function (event) {
     const value = event.target.value;
     console.log('color scheme changed to', value);
@@ -100,3 +101,40 @@ contactForm?.addEventListener('submit', function (event) {
     }
     location.href = url;
 });
+
+
+
+export async function fetchJSON(url) {
+    try {
+        // Fetch the JSON file from the given URL
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Failed to fetch projects: ${response.statusText}`);
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching or parsing JSON data:', error);
+    }
+}
+
+
+
+export function renderProjects(projects, containerElement, headingLevel = 'h2') {
+    containerElement.innerHTML = '';
+    for (let project of projects) {
+        const article = document.createElement('article');
+        article.innerHTML = `
+            <h3>${project.title}</h3>
+            <img src=${project.image} alt="${project.title}" />
+            <p>${project.description}</p>
+        `;
+        containerElement?.appendChild(article);
+    }
+}
+
+
+
+export async function fetchGithubData(username) {
+    return fetchJSON(`https://api.github.com/users/${username}`);
+}
